1f7f6777f71ee79b38abc9e288b77d963502ad94
Fuyuki MATSUYAMA <fuyumaz@gmail.com>
2013-02-17 02:06:19 +0900

    Bug fix01

--- css3transX.js
+++ css3transX.js
@@ -62,7 +62,7 @@ var css3transX = {
 		var t = this;
 		var elm = targetElm[0];
 		elm.style[t.param[0]] = t.param[2]+ t.easeArray[easeType];
-		elm.style[t.param[1]] = t.param[4] + targetPosX + t.param[5];
+		elm.style[t.param[2]] = t.param[4] + targetPosX + t.param[5];
 		
 		targetElm.one(t.param[6], function(e){// depend jQuery
 			elm.style.removeProperty(t.param[0]);//rest animation
